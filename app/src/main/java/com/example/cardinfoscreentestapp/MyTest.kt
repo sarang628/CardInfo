a3f6e2ac2f6dd815a83c3a3b5e779edb1f67846e
SRYANG <sarang628@naver.com>
2023-05-05 18:52:20 +0800

    update

---
+++ app/src/main/java/com/example/cardinfoscreentestapp/MyTest.kt
@@ -0,0 +1,10 @@
+package com.example.cardinfoscreentestapp
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+class MyTest {
+}
