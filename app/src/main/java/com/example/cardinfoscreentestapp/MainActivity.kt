90bb17b8fec19dc2fe4c223354f7ecd69e0a5d2c
SRYANG <sarang628@naver.com>
2023-05-12 17:25:38 +0800

    update

--- app/src/main/java/com/example/cardinfoscreentestapp/MainActivity.kt
+++ app/src/main/java/com/example/cardinfoscreentestapp/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.cardinfoscreentestapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.ui.Modifier
-import com.example.cardinfo.RestaurantCardPage
-import com.example.cardinfo.testRestaurantInfoCardUiState
+import com.example.cardinfo.TestRestaurantCard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,14 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val d = testRestaurantInfoCardUiState(this)
         setContent {
-            Box {
-                Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Bottom) {
-                    RestaurantCardPage(d)
-                }
-            }
+            TestRestaurantCard(this)
         }
-
     }
 }
---
+++ library/src/main/java/com/example/cardinfo/TestRestaurantCard.kt
@@ -0,0 +1,20 @@
+package com.example.cardinfo
+
+import android.content.Context
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TestRestaurantCard(context: Context) {
+    val d = testRestaurantInfoCardUiState(context)
+    Box {
+        Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Bottom) {
+            RestaurantCardPage(d)
+        }
+    }
+}
