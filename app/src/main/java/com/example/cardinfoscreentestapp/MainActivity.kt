93ce87797542d7ad0b8ad0cd59e126d79aade69b
SRYANG <sarang628@naver.com>
2023-10-26 17:29:21 +0800

    update

--- app/src/main/java/com/example/cardinfoscreentestapp/MainActivity.kt
+++ app/src/main/java/com/example/cardinfoscreentestapp/MainActivity.kt
@@ -10,12 +10,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.cardinfo.RestaurantCardData
 import com.example.cardinfo.RestaurantCardPage
 import com.example.cardinfo.RestaurantCardViewModel
+import com.example.cardinfo.getTestRestaurnat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,21 +32,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var visible by remember { mutableStateOf(false) }
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = "cardInfo") {
                 composable("cardInfo") {
                     Box {
                         Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Bottom) {
                             RestaurantCardPage(
-                                uiState = viewModel.uiState,
                                 restaurantImageUrl = "http://sarang628.iptime.org:89/restaurant_images/",
                                 onChangePage = {},
-                                onClickCard = { navController.navigate("detail") }
+                                onClickCard = { navController.navigate("detail") },
+                                restaurants = ArrayList<RestaurantCardData>().apply {
+                                    add(getTestRestaurnat())
+                                    add(getTestRestaurnat())
+                                    add(getTestRestaurnat())
+                                    add(getTestRestaurnat())
+                                    add(getTestRestaurnat())
+                                },
+                                visible = visible
                             )
                         }
                         Button(onClick = {
-
+                            visible = !visible
                         }) {
+                            Text(text = "hide")
                         }
                     }
                 }
--- library/src/main/java/com/example/cardinfo/RestaurantCardPage.kt
+++ library/src/main/java/com/example/cardinfo/RestaurantCardPage.kt
@@ -1,23 +1,42 @@
 package com.example.cardinfo
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.StateFlow
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun RestaurantCardPage(
-    uiState: StateFlow<RestaurantInfoCardUiState>,
     onChangePage: ((Int) -> Unit)? = null,
     restaurants: List<RestaurantCardData>? = null, // 현재 검색된 맛집리스트
     selectedRestaurant: RestaurantCardData? = null, // 현재 선택된 맛집
     restaurantImageUrl: String,
-    onClickCard: (Int) -> Unit
+    onClickCard: (Int) -> Unit,
+    visible: Boolean
 ) {
     val pageState = rememberPagerState()
 
@@ -45,18 +64,29 @@ fun RestaurantCardPage(
         }
     })
 
+    val density = LocalDensity.current
+    AnimatedVisibility(
+        visible = visible,
+        enter = slideInVertically { with(density) { 200.dp.roundToPx() } },
+        exit = slideOutVertically { with(density) { 200.dp.roundToPx() } }
+    ) {
+        Column {
+            // 데이터가 없을 때도 onPageChange가 발생해 데이터가 있을때 그리도록 변경
+            if (!restaurants.isNullOrEmpty()) {
 
-    // 데이터가 없을 때도 onPageChange가 발생해 데이터가 있을때 그리도록 변경
-    if (!restaurants.isNullOrEmpty()) {
-        HorizontalPager(
-            pageCount = restaurants.size,
-            state = pageState,
-        ) { page ->
-            RestaurantCard(
-                restaurant = restaurants[page],
-                restaurantImageUrl = restaurantImageUrl,
-                onClickCard = onClickCard
-            )
+                HorizontalPager(
+                    pageCount = restaurants.size,
+                    state = pageState,
+                ) { page ->
+                    RestaurantCard(
+                        restaurant = restaurants[page],
+                        restaurantImageUrl = restaurantImageUrl,
+                        onClickCard = onClickCard
+                    )
+                }
+            }
         }
     }
+
+
 }
