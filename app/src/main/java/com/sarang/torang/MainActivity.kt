dbb86e7e877f5f6341131d72175ca7fb19f665f5
sryang <sarang628@gmail.com>
2025-08-08 11:38:20 +0800

    add card and list interaction

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -6,53 +6,93 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.sarang.torang.compose.cardinfo.CardInfoImageLoader
 import com.sarang.torang.compose.cardinfo.LocalCardInfoImageLoader
-import com.sarang.torang.compose.cardinfo.RestaurantCardData
-import com.sarang.torang.compose.cardinfo.RestaurantCardPage1
+import com.sarang.torang.compose.cardinfo.RestaurantCardUIState
+import com.sarang.torang.compose.cardinfo.RestaurantCardPage
 import com.sarang.torang.compose.cardinfo.getTestRestaurantCardData
 import com.sarang.torang.di.image.provideTorangAsyncImage
+import com.sarang.torang.di.repository.repository.impl.FindRepositoryImpl
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var findRepository : FindRepositoryImpl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             var visible by remember { mutableStateOf(false) }
+            val coroutineScope = rememberCoroutineScope()
             val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "cardInfo") {
-                composable("cardInfo") {
-                    Box {
-                        Column(
-                            Modifier.Companion.fillMaxHeight(),
-                            verticalArrangement = Arrangement.Bottom
-                        ) {
-                            CompositionLocalProvider(LocalCardInfoImageLoader provides customImageLoader) {
-                                RestaurantCardPage1(
-                                    onClickCard = { navController.navigate("detail") },
-                                    restaurants = ArrayList<RestaurantCardData>().apply { add(getTestRestaurantCardData1());add(getTestRestaurantCardData1());add(getTestRestaurantCardData1());add(getTestRestaurantCardData1());add(getTestRestaurantCardData1()) },
-                                    visible = visible,
-                                )
-                            }
+            val restaurants = findRepository.restaurants.collectAsState().value
+            val selectedRestaurant = findRepository.selectedRestaurant.collectAsState().value
+            val state: LazyListState = rememberLazyListState()
+
+            LaunchedEffect(selectedRestaurant) {
+                findRepository.restaurants.collect { list ->
+                    list.forEachIndexed { index, restaurant ->
+                        if (restaurant.restaurantId == selectedRestaurant.restaurantId) {
+                            state.animateScrollToItem(index)
                         }
-                        Button(onClick = { visible = !visible }) { Text(text = "hide") }
                     }
                 }
-                composable("detail") { Text(text = "test") }
+            }
+
+            Scaffold {
+                NavHost(modifier = Modifier.padding(it), navController = navController, startDestination = "cardInfo") {
+                    composable("cardInfo") {
+                        Box {
+                            Column(modifier = Modifier.Companion.fillMaxHeight(), verticalArrangement = Arrangement.Bottom) {
+                                LazyColumn(modifier = Modifier.height(400.dp), state = state) {
+                                    items(restaurants.size) {
+                                        TextButton({ coroutineScope.launch { restaurants[it].restaurantId?.let{findRepository.selectRestaurant(it)} } }) {
+                                            Text(("${restaurants[it].restaurantName}"))
+                                        }
+                                    }
+                                }
+                                CompositionLocalProvider(LocalCardInfoImageLoader provides customImageLoader) {
+                                    RestaurantCardPage(
+                                        onClickCard = { navController.navigate("detail") },
+                                        visible = visible,
+                                    )
+                                }
+                            }
+                            Row {
+                                Button(onClick = { visible = !visible }) { Text(text = if(visible) "hide" else "show") }
+                                Button(onClick = { coroutineScope.launch { findRepository.findFilter() } }) { Text(text = "get Restaurants") }
+                            }
+                        }
+                    }
+                    composable("detail") { Text(text = "test") }
+                }
             }
         }
     }
@@ -62,7 +102,7 @@ class MainActivity : AppCompatActivity() {
         provideTorangAsyncImage().invoke(modifier, url, width, height, scale)
     }
 
-    fun getTestRestaurantCardData1(): RestaurantCardData {
+    fun getTestRestaurantCardData1(): RestaurantCardUIState {
         val data = getTestRestaurantCardData()
         return data.copy(restaurantImage = BuildConfig.RESTAURANT_IMAGE_SERVER_URL + data.restaurantImage)
     }
